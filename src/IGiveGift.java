aacb0cd270c776bdf51bd3135e962bca62974d17
singworld <xiaoxinbian@gmail.com>
2018-07-12 17:18:20 +0800

    dahuaproxy

---
+++ src/IGiveGift.java
@@ -0,0 +1,5 @@
+public interface IGiveGift {
+    void GiveDolls();
+    void GiveFlowers();
+    void GiveChocolate();
+}
---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        SchoolGirl jiaojiao = new SchoolGirl();
+        jiaojiao.setName("李娇娇");
+        Proxy daili = new Proxy(jiaojiao);
+        daili.GiveDolls();
+        daili.GiveFlowers();
+        daili.GiveChocolate();
+    }
+}
