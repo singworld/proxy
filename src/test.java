f106e7cf1ce24418ed256ea5638bb02a508a9b40
singworld <xiaoxinbian@gmail.com>
2018-07-12 17:49:58 +0800

    test修改 test2新增

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         SchoolGirl jiaojiao = new SchoolGirl();
-        jiaojiao.setName("李娇娇");
+        jiaojiao.setName("李娇");
         Proxy daili = new Proxy(jiaojiao);
         daili.GiveDolls();
         daili.GiveFlowers();
